797cec035742eb6a1352d055ea4d89841562defa
Yurii Andrusiak <yurii.andrusiak@gmail.com>
2022-09-08 14:10:47 +0300

    Fix rendering on apps

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
-// import ReactDOM from 'react-dom';
 import App from './App';
-import Monobank from './Monobank';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App  />);
+root.render(<App />);
